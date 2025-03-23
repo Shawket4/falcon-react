767b30ccc543c585af621491b60ed75de5684fa9
Shawket Ibrahim <shawket@Shawkets-MacBook-Pro.local>
2025-03-23 17:44:53 +0200

    fixed serverip

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const SERVER_IP = "http://localhost:3001";
-// export const SERVER_IP = "https://apextransport.ddns.net:3001";
+// export const SERVER_IP = "http://localhost:3001";
+export const SERVER_IP = "https://apextransport.ddns.net:3001";

adcec77b7b234b83c6e5ca7a47e55246a28b18de
Shawket Ibrahim <shawket@Shawkets-MacBook-Pro.local>
2025-04-28 22:26:08 +0300

    Added driver and vehicle forms

--- src/components/AddCar.jsx
+++ src/components/AddCar.jsx
@@ -235,6 +235,7 @@ const AddCar = () => {
                   onChange={(e) => handleCompartmentChange(index, e.target.value)}
                   className="mt-1 block w-full py-2 px-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
                   placeholder="Enter size"
+                  required
                 />
               </div>
             ))}
